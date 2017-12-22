78fb973d22d6f91d0f4987b4d61ef3c1b4531a32
Kübra Elif TOZKOPARAN <32227641+Keett@users.noreply.github.com>
2017-12-22 16:43:54 +0300

    model: Ayarlar
    veritabanı ve Ayarlar tablosu oluşturuldu, oluşturulan tabloya veri eklendi.

---
+++ Ayarlar.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ayarlar extends Model
+{
+    protected $table='ayarlar';
+    protected $fillable=['logo','url','title','description','keywords','author','tel','gsm','faks','mail','adres','il','ilce','recepctha','maps','analystic','facebook','twitter','instagram','youtube','google','smtp_user','smtp_password','smtp_host','smtp_port'];
+
+}
